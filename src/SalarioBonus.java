2dd5099d052b668ed1e2fe60f43622f2ce2ad708
Eduardo Morais <duudimorais@gmail.com>
2025-02-26 13:51:59 -0300

    Correção de pula linha no final

--- src/SalarioBonus.java
+++ src/SalarioBonus.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class SalarioBonus {
+import java.util.Scanner;
+public class SalarioBonus {
+    public static void main (String []args) {
+
+        String nome;
+        double salario, comissao, vendas, total;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        nome = teclado.nextLine();
+
+        System.out.println("Digite o seu salario fixo: ");
+        salario = teclado.nextDouble();
+
+        System.out.println("Digite o quanto vendeu: ");
+        vendas = teclado.nextDouble();
+
+        comissao = vendas * (0.15);
+
+        total = comissao + salario;
+
+        System.out.printf("TOTAL = R$ %.2f\n", total);
+
+    }
 }
