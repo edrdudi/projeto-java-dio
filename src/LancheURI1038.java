75133d2c8872606550c3920092747bb703264333
Eduardo Morais <duudimorais@gmail.com>
2025-03-03 20:46:24 -0300

    Otimização no código

--- src/LancheURI1038.java
+++ src/LancheURI1038.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class LancheURI1038 {
+import java.util.Scanner;
+
+public class LancheURI1038 {
+    public static void main (String []args) {
+        Scanner teclado = new Scanner(System.in);
+
+        double preco;
+        preco = 0;
+
+        int codigo = teclado.nextInt();
+        double qtdItem = teclado.nextDouble();
+
+        switch (codigo) {
+            case 1:
+                preco = qtdItem * 4;
+                break;
+
+            case 2:
+                preco = qtdItem * 4.5;
+                break;
+
+            case 3:
+                preco = qtdItem * 5;
+                break;
+
+            case 4:
+                preco = qtdItem * 2;
+                break;
+
+            case 5:
+                preco = qtdItem * 1.5;
+                break;
+        }
+
+        System.out.printf("Total: R$ %.2f\n", preco);
+    }
 }
