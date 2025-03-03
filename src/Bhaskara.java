bbe6963cfe682580a1b216c954edb163e77896ad
Eduardo Morais <duudimorais@gmail.com>
2025-03-03 20:50:04 -0300

    correão de commits de outras classes

--- src/Bhaskara.java
+++ src/Bhaskara.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Bhaskara {
+import java.util.Scanner;
+
+public class Bhaskara {
+    public static void main (String args []) {
+        Scanner teclado = new Scanner(System.in);
+
+        double R1, R2, delta;
+
+        double A = teclado.nextDouble();
+        double B = teclado.nextDouble();
+        double C = teclado.nextDouble();
+
+        delta = B * B - 4 * A * C;
+
+        if (delta < 0 || A == 0) {
+            System.out.println("Impossivel calcular");
+        } else {
+            R1 = (-B + Math.sqrt(delta)) / (2 * A);
+            R2 = (-B - Math.sqrt(delta)) / (2 * A);
+
+            System.out.printf("R1 = %.5f\n", R1);
+            System.out.printf("R2 = %.5f\n", R2);
+        }
+    }
 }
--- src/TesteSelecao1.java
+++ src/TesteSelecao1.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class TesteSelecao1 {
+import java.util.Scanner;
+
+public class TesteSelecao1 {
+    public static void main (String args[]) {
+        Scanner telcado = new Scanner(System.in);
+
+        int A = telcado.nextInt();
+        int B = telcado.nextInt();
+        int C = telcado.nextInt();
+        int D = telcado.nextInt();
+
+        int soma1 = C + D;
+        int soma2 = A + B;
+
+        if (B > C && D > A && soma1 > soma2 && C > 0 && D > 0 && A % 2 == 0) {
+            System.out.println("Valor aceitos");
+        } else {
+            System.out.println("Valores não aceitos");
+        }
+    }
 }
