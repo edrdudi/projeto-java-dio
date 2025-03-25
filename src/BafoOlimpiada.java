793a3de88e40b031d0682d05821f15592f0de1de
Eduardo Morais <duudimorais@gmail.com>
2025-03-24 21:31:10 -0300

    Resolução do exercício de bafo da Olimpiada de Computação

---
+++ src/BafoOlimpiada.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class BafoOlimpiada {
+    public static void main (String [] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int R, A, B;
+        int contador;
+        int teste = 0;
+        int somaA = 0;
+        int somaB = 0;
+
+        do {
+            R = teclado.nextInt();
+
+            for (contador = 0; contador < R; contador++) {
+
+                A = teclado.nextInt();
+                B = teclado.nextInt();
+
+                somaA = somaA + A;
+                somaB = somaB + B;
+
+            }
+            if (R != 0) {
+                teste++;
+                System.out.println("Teste " + teste);
+
+                if (somaA < somaB) {
+                    System.out.println("Beto");
+                } else {
+                    System.out.println("Aldo");
+                }
+
+                System.out.println("\n");
+            }
+
+        } while (R != 0);
+    }
+}
